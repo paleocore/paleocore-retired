5a73c7f1a066785e822f8f5e30c9a3c5a950faf2
Tomislav Urban <tomislav.urban@gmail.com>
2014-04-10 11:16:06 -0500

    Update fiberPermissions.py
    Removing unused imports

--- mysite/fiberPermissions.py
+++ mysite/fiberPermissions.py
@@ -6,10 +6,6 @@
 """
 
 from django.contrib.auth.models import User
-from django.db.models import Q
-from django.contrib.contenttypes.models import ContentType
-from django.db.models.signals import pre_delete
-
 from fiber.permissions import Permissions
 from fiber.models import Image, File, Page, PageContentItem, ContentItem
 
