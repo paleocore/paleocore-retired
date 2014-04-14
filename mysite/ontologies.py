b51b338b15e152a97b774f038186c3cfefbe8adf
Denne Reed <denne.reed@gmail.com>
2014-04-14 15:43:43 -0500

    Removed unused fields from mlp Occurrence Model, added ontologies

---
+++ mysite/ontologies.py
@@ -0,0 +1,19 @@
+
+BASIS_OF_RECORD_VOCABULARY = (("FossilSpecimen", "Fossil"), ("HumanObservation", "Observation"))
+ITEM_TYPE_VOCABULARY = (("Artifactual", "Artifactual"), ("Faunal", "Faunal"), ("Floral", "Floral"), ("Geological", "Geological"))
+COLLECTING_METHOD_VOCABULARY = (("Surface Standard", "Surface Standard"),
+                                ("Surface Intensive", "Surface Intensive"),
+                                ("Surface Complete", "Surface Complete"),
+                                ("Exploratory Survey", "Exploratory Survey"),
+                                ("Dry Screen 5mm", "Dry Screen 5mm"),
+                                ("Dry Screen 2mm","Dry Screen 2mm"),
+                                ("Wet Screen 1mm", "Wet Screen 1mm")
+)
+
+COLLECTOR_CHOICES = (("Zeresenay Alemseged", "Zeresenay Alemseged"),
+                     ("Rene Bobe", "Rene Bobe"),
+                     ("Denis Geraads", "Denis Geraads"),
+                     ("Shannon McPherron", "Shannon McPherron"),
+                     ("Denne Reed", "Denne Reed"),
+                     ("Jonathan Wynn", "Jonathan Wynn")
+)
