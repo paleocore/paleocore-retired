3c8b8ba951f663c0d07c7c160e4471b07192d87f
turban <tomislav.urban@gmail.com>
2014-04-18 14:27:45 -0500

    Moving location of banner images. (again)

--- base/templatetags/banner.py
+++ base/templatetags/banner.py
@@ -7,7 +7,6 @@ register = template.Library()
 
 @register.filter(name='bannerImage')
 def bannerImage(value):
-    banner_images_path = path.join(settings.BASE_DIR, "base", "static", "banner_images")
+    banner_images_path = path.join(settings.BASE_DIR, "static", "base", "banner_images")
     banner_images_names = listdir(banner_images_path)
     return choice(banner_images_names)
-
