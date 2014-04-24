f3ab647c895bf323c5668b280bb74c2e7c96f464
Denne Reed <denne.reed@gmail.com>
2014-04-24 17:32:00 -0500

    Added new tests for data app Added fiber data fixtures for testing

--- data/tests.py
+++ data/tests.py
@@ -2,25 +2,50 @@ from django.test import TestCase
 from data.models import Project
 from django.core.urlresolvers import reverse
 
-# Create your tests here.
+
+# # Factory method to create a fiber page tree with five pages.
+# def create_django_page_tree():
+#     mainmenu=Page(title='mainmenu')
+#     mainmenu.save()
+#     home=Page(title='home', parent=mainmenu, url='home', template_name='base/home.html')
+#     home.save()
+#     join=Page(title='join', parent=home, url='join', template_name='base/join.html')
+#     join.save()
+#     members=Page(title='members', parent=home, url='members', template_name='base/members')
+#     members.save()
+#     meetings = Page(title='meetings', parent=mainmenu, url='meetings', template_name='')
+#     meetings.save()
+
 
 class ProjectIndexViewTests(TestCase):
-    def test_index_view_with_no_projects(self):
-        """
-        If no projects an appropriate mesage should be displayed
-        """
-        response = self.client.get(reverse('data:index'))
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "No projects are available")
-        self.assertQuerysetEqual(response.context['project_list'], [])
+    fixtures = ['fixtures/fiber_test_data.json']
 
-    def test_index_view_with_one_project(self):
+    def test_data_index_view(self):
         """
-        If a project is present should list it.
+        Index view should show fiber content and a list of projects in the nav bar
         """
-        create_project(title="Test Project", short_title="TP")
-        response = self.client.get(reverse('data:index'))
-        self.assertQuerysetEqual(
-            response.context['project_list'],
-            ['<Project: Test Project>']
-        )
+        response = self.client.get(reverse('data:index'))  # get the index page
+        self.assertEqual(response.status_code, 200)  # status code should be 200
+        self.assertContains(response, "Dikika Research Project")
+        self.assertContains(response, "Mille-Logya Research Project")
+
+
+    # def test_index_view_with_no_projects(self):
+    #     """
+    #     If no projects an appropriate message should be displayed
+    #     """
+    #     response = self.client.get(reverse('data:index'))
+    #     self.assertEqual(response.status_code, 200)
+    #     #self.assertContains(response, "No projects are available")
+    #     #self.assertQuerysetEqual(response.context['project_list'], [])
+    #
+    # def test_index_view_with_one_project(self):
+    #     """
+    #     If a project is present should list it.
+    #     """
+    #     create_project(title="Test Project", short_title="TP")
+    #     response = self.client.get(reverse('data:index'))
+    #     self.assertQuerysetEqual(
+    #         response.context['project_list'],
+    #         ['<Project: Test Project>']
+    #     )
