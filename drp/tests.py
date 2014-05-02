0f2ed2e667a9409fced380fcb5ce771b876f3f0d
wabarr <wabarr@gmail.com>
2014-05-02 16:38:38 -0500

    new test

--- drp/tests.py
+++ drp/tests.py
@@ -1,5 +1,17 @@
 from django.test import TestCase
 from drp.models import *
-from fiber.models import Page
+from django.contrib.gis.geos import Point
+
+class AddDeleteOccurrence(TestCase):
+    def setUp(self):
+        newRecord = drp_occurrence(geom=Point(1,1))
+        newRecord.save()
+
+    def test_record_exists(self):
+        nrecords = drp_occurrence.objects.all().count()
+        self.assertEqual(nrecords, 1)
 
 
+
+class ProjectIndexViewTests(TestCase):
+    fixtures = ['fixtures/fiber_test_data.json']
