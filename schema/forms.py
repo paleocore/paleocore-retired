a77f73f6cd530d620833c427e35e97d0a50c8996
turban <tomislav.urban@gmail.com>
2014-05-19 11:43:59 -0500

    Adding forms.py to git.

---
+++ schema/forms.py
@@ -0,0 +1,26 @@
+from django import forms
+from schema.models import Project, TermCategory
+
+SHOW_COLUMNS = (
+    ('definition','Field Definitions'),
+    ('type','Field Types'),
+)
+
+class TermViewForm(forms.Form):
+    baseProject = forms.ModelChoiceField(Project.objects.all(), label="Base Project:")
+    showColumns = forms.MultipleChoiceField(required=False,label="Show Columns:", widget=forms.CheckboxSelectMultiple(), choices=SHOW_COLUMNS,)
+    showProjects = forms.ModelMultipleChoiceField(Project.objects.all(), label="Show Projects:", widget=forms.CheckboxSelectMultiple())
+    showCategories = forms.ModelMultipleChoiceField(TermCategory.objects.all(), required=False, label="Show Categories:", widget=forms.CheckboxSelectMultiple())
+
+
+class RelateProjectsForm(forms.Form):
+    firstProjectList = forms.ModelChoiceField(Project.objects.all(), label="Project One:")
+    secondProjectList = forms.ModelChoiceField(Project.objects.all(), label="Project Two:")
+
+class RelateTermsForm(forms.Form):
+
+    def __init__(self, firstProject, secondProject, *args, **kwargs):
+        super(RelateTermsForm, self).__init__(*args, **kwargs)
+
+        self.fields['firstProjectTerms'] = forms.ModelMultipleChoiceField(firstProject.term_set.all())
+        self.fields['secondProjectTerms'] = forms.ModelMultipleChoiceField(secondProject.term_set.all())
