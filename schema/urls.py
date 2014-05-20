94215b91cdb9cf635e5186e78f628c41de8af2ee
wabarr <wabarr@gmail.com>
2014-05-20 11:33:16 -0500

    give urls names to call in template with {% url %}

--- schema/urls.py
+++ schema/urls.py
@@ -3,12 +3,12 @@ from schema import views
 
 
 urlpatterns = patterns('',
-    url(r'^terms/$', 'schema.views.terms'),
-    url(r'^term/(?P<id>\d+)', 'schema.views.term'),
-    url(r'^ontology/(?P<category>\d+)','schema.views.classes'),
-    url(r'ontologyTree/$', 'schema.views.ontologyTree'),
-    url(r'ontologyTree/(?P<categoryID>\d+)','schema.views.ontologyTree'),
-    url(r'^ontologyJSONtree/$', 'schema.views.ontologyJSONtree'),
+    url(r'^terms/$', 'schema.views.terms', name="termNoParameter"),
+    url(r'^term/(?P<id>\d+)', 'schema.views.term', name="term"),
+    url(r'^ontology/(?P<category>\d+)','schema.views.classes', name="ontologyClass"),
+    url(r'ontologyTree/$', 'schema.views.ontologyTree', name="ontologyTreeNoParameter"),
+    url(r'ontologyTree/(?P<categoryID>\d+)','schema.views.ontologyTree', name="ontologyTree"),
+    url(r'^ontologyJSONtree/$', 'schema.views.ontologyJSONtree', name="ontologyJSONtree"),
 )
 
 # From IndexView of meetings app
