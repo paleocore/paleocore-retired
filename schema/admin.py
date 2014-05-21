77481756eb6f5d1d80d7ebdc651ca88ea3fd301e
wabarr <wabarr@gmail.com>
2014-05-21 10:53:34 -0500

    added filters and search for term relationships

--- schema/admin.py
+++ schema/admin.py
@@ -17,6 +17,38 @@ class TermCategoryAdmin(admin.ModelAdmin):
     list_filter = ['is_occurrence']
     ordering = ('name',)
 
+
+#custom admin list filter. this comes straight from the django admin site docs
+#I could add related_term__project to list filters, but this would result in two filters called "project"
+#I get around this by creating a custom SimpleListFilter so I can specify the name
+class RelatedProjectsListFilter(admin.SimpleListFilter):
+    # Human-readable title which will be displayed in the
+    # right admin sidebar just above the filter options.
+    title = 'related project'
+
+    # Parameter for the filter that will be used in the URL query.
+    parameter_name = 'related-project'
+
+    def lookups(self, request, model_admin):
+        """
+        Returns a list of tuples. The first element in each
+        tuple is the coded value for the option that will
+        appear in the URL query. The second element is the
+        human-readable name for the option that will appear
+        in the right sidebar.
+        """
+        projectsTuple = tuple(Project.objects.all().values_list("name","name"))
+        return projectsTuple
+
+    def queryset(self, request, queryset):
+        """
+        Returns the filtered queryset based on the value
+        provided in the query string and retrievable via
+        `self.value()`.
+        """
+        if self.value():
+            return queryset.filter(related_term__project__name=self.value())
+
 class TermRelationshipAdmin(admin.ModelAdmin):
     def term(self):
         project_name = str(self.term.project)
@@ -29,6 +61,8 @@ class TermRelationshipAdmin(admin.ModelAdmin):
         return project_name+' : '+term_name
 
     list_display = (term, related_term, 'relationship_type','term_project')
+    list_filter = ["term__project", RelatedProjectsListFilter]
+    search_fields = ["related_term__name", "term__name"]
     ordering = ('term',)
 
 admin.site.register(Project, ProjectAdmin)
