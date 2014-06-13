9adeb3da9db106fd134fbfe11cd621ed71960fa4
dennereed <denne.reed@gmail.com>
2014-06-13 17:31:32 -0500

    Updated Donwload .csv admin action in drp app

--- drp/admin.py
+++ drp/admin.py
@@ -128,28 +128,85 @@ class occurrenceAdmin(admin.ModelAdmin):
     actions = ["get_nearest_locality", "create_data_csv"]
 
     #admin action to download data in csv format
+    # def create_data_csv(self, request, queryset):
+    #     response = HttpResponse(content_type='text/csv')
+    #     response['Content-Disposition'] = 'attachment; filename="DRP_data.csv"'
+    #     writer = unicodecsv.writer(response)
+    #     o = drp_occurrence()  # create an empty instance of an occurrence object
+    #     b = drp_biology()  # create an empty instance of a biology object
+    #
+    #     """
+    #     For now this method handles all occurrences and corresponding
+    #     data from the biology table for faunal occurrences.
+    #     """
+    #     writer.writerow(o.__dict__.keys()+b.__dict__.keys())  # fetch field names and write to the first row
+    #
+    #     for occurrence in queryset.all():  # iterate through the occurrence instances selected in the admin
+    #         try:  # try writing drp_occurrence values and associated drp_biology values
+    #             writer.writerow(occurrence.__dict__.values()+occurrence.drp_biology.__dict__.values())
+    #         except ObjectDoesNotExist:  # Django specific exception
+    #             writer.writerow(occurrence.__dict__.values())  # add only drp_occurrence values)
+    #         except:
+    #             writer.writerow(occurrence.__dict__.values())  # add only drp_occurrence values
+    #
+    #     return response
+    #
+
     def create_data_csv(self, request, queryset):
-        response = HttpResponse(content_type='text/csv')
-        response['Content-Disposition'] = 'attachment; filename="DRP_data.csv"'
-        writer = unicodecsv.writer(response)
+        response = HttpResponse(content_type='text/csv')  # declare the response type
+        response['Content-Disposition'] = 'attachment; filename="DRP_data.csv"'  # declare the file name
+        writer = unicodecsv.writer(response)  # open a .csv writer
         o = drp_occurrence()  # create an empty instance of an occurrence object
         b = drp_biology()  # create an empty instance of a biology object
 
-        """
-        For now this method handles all occurrences and corresponding
-        data from the biology table for faunal occurrences.
-        """
-        writer.writerow(o.__dict__.keys()+b.__dict__.keys())  # fetch field names and write to the first row
+        occurrence_field_list = o.__dict__.keys()  # fetch the fields names from the instance dictionary
+        try:  # try removing the state field from the list
+            occurrence_field_list.remove('_state')  # remove the _state field
+        except ValueError:  # raised if _state field is not in the dictionary list
+            pass
+        try:  # try removing the geom field from the list
+            occurrence_field_list.remove('geom')
+        except ValueError:  # raised if geom field is not in the dictionary list
+            pass
+        # Replace the geom field with two new fields
+        occurrence_field_list.append("point_x")  # add new fields for coordinates of the geom object
+        occurrence_field_list.append("point_y")
 
-        for occurrence in queryset.all():  # iterate through the occurrence instances selected in the admin
-            try:  # try writing drp_occurrence values and associated drp_biology values
-                writer.writerow(occurrence.__dict__.values()+occurrence.drp_biology.__dict__.values())
+        biology_field_list = b.__dict__.keys()  # get biology fields
+        try:  # try removing the state field
+            biology_field_list.remove('_state')
+        except ValueError:  # raised if _state field is not in the dictionary list
+            pass
+
+        #################################################################
+        # For now this method handles all occurrences and corresponding #
+        # data from the biology table for faunal occurrences.           #
+        #################################################################
+        writer.writerow(occurrence_field_list+biology_field_list)  # write column headers
+
+        for occurrence in queryset:  # iterate through the occurrence instances selected in the admin
+            # The next line uses string comprehension to build a list of values for each field
+            occurrence_dict = occurrence.__dict__
+            occurrence_dict['point_x'] = occurrence.geom.get_x()  # translate the occurrence geom object
+            occurrence_dict['point_y'] = occurrence.geom.get_y()
+
+            # Next we use the field list to fetch the values from the dictionary.
+            # Dictionaries do not have a reliable ordering. This code insures we get the values
+            # in the same order as the field list.
+            try:  # Try writing values for all keys listed in both the occurrence and biology tables
+                writer.writerow([occurrence.__dict__.get(k) for k in occurrence_field_list] +
+                                [occurrence.Biology.__dict__.get(k) for k in biology_field_list])
             except ObjectDoesNotExist:  # Django specific exception
-                writer.writerow(occurrence.__dict__.values())  # add only drp_occurrence values)
-            except:
-                writer.writerow(occurrence.__dict__.values())  # add only drp_occurrence values
+                writer.writerow([occurrence.__dict__.get(k) for k in occurrence_field_list])
+            except AttributeError:  # Django specific exception
+                writer.writerow([occurrence.__dict__.get(k) for k in occurrence_field_list])
 
         return response
+
+
+
+
+
 
     create_data_csv.short_description = "Download Selected to .csv"
 
