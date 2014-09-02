f46e3eb31e14a6754078af53f8ceadd735fcbff3
Denne Reed <denne.reed@gmail.com>
2014-09-02 10:26:44 -0500

    Updated terms admin to show term id in list view.

--- standard/admin.py
+++ standard/admin.py
@@ -2,18 +2,21 @@ from django.contrib import admin
 from standard.models import Project, Term, TermCategory, TermStatus, TermDataType, TermRelationship, \
         TermRelationshipType, Comment
 
+
 class ProjectAdmin(admin.ModelAdmin):
     list_display = ('name', 'user_names', 'geographic', 'temporal')
     filter_horizontal = ('users',)
     ordering = ("name",)
 
+
 class TermAdmin(admin.ModelAdmin):
-    list_display = ('name','project','data_type','status','category')
-    list_filter = ['project','data_type','status','category']
-    ordering = ('project','name')
+    list_display = ('id', 'name', 'project', 'data_type', 'status', 'category')
+    list_filter = ['project', 'data_type', 'status', 'category']
+    ordering = ('project', 'name')
+
 
 class TermCategoryAdmin(admin.ModelAdmin):
-    list_display = ('name', 'is_occurrence', 'description','parent','tree_visibility')
+    list_display = ('name', 'is_occurrence', 'description', 'parent', 'tree_visibility')
     list_filter = ['is_occurrence']
     ordering = ('name',)
 
@@ -60,7 +63,7 @@ class TermRelationshipAdmin(admin.ModelAdmin):
         term_name  =str(self.related_term.name)
         return project_name+' : '+term_name
 
-    list_display = (term, related_term, 'relationship_type','term_project')
+    list_display = (term, related_term, 'relationship_type', 'term_project')
     list_filter = ["term__project", RelatedProjectsListFilter]
     search_fields = ["related_term__name", "term__name"]
     ordering = ('term',)
