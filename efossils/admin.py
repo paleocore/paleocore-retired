50e8242ceb8ba573946c1f3be161477eb66dad40
Denne Reed <denne.reed@gmail.com>
2014-09-26 16:00:56 -0500

    initial configuration of models.py and admin.py for efossils app.

--- efossils/admin.py
+++ efossils/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from models import PaleoSite
+
 
 # Register your models here.
+
+
+class PaleoSiteAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'setting')
+
+admin.site.register(PaleoSite, PaleoSiteAdmin)
