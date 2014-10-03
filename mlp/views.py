ac5cb283901bf9205655ff3dfa08abe08fac79bc
turban <tomislav.urban@gmail.com>
2014-10-03 17:30:10 -0500

    Added looping to KML import.

--- mlp/views.py
+++ mlp/views.py
@@ -16,6 +16,7 @@ import django.contrib.gis
 import datetime
 from django.contrib.gis.gdal import DataSource
 from django.contrib.gis.geos import GEOSGeometry
+import utm
 
 class UploadKMLView(FiberPageMixin, generic.FormView):
     template_name = 'mlp/upload_kml.html'
@@ -36,34 +37,35 @@ class UploadKMLView(FiberPageMixin, generic.FormView):
         kmldoc = kmlf.read()  # read() loads entire file as one string
         kmlf.close()
         k.from_string(kmldoc)
-        features = k._features
 
-        f2 = list(features[0].features())
-        firstFeature = f2[1]
-        firstFeatureName = firstFeature.name
-        firstFeatureDescription = firstFeature.description
-        table = etree.fromstring(firstFeatureDescription)
-        attributes = table.xpath("//text()")
-        # TODO test attributes is even length
-        attributes_dict = dict(zip(attributes[0::2], attributes[1::2]))
+        features = list(k._features[0]._features)
+        #chop off the folder feature
+        occurrences = features[1:len(features)-1]
+        for f in occurrences:
+            table = etree.fromstring(f.description)
+            attributes = table.xpath("//text")
+            # TODO test attributes is even length
+            attributes_dict = dict(zip(attributes[0::2], attributes[1::2]))
 
-        mlp_occ = Occurrence()
+            mlp_occ = Occurrence()
 
-        mlp_occ.barcode = attributes_dict.get("Barcode")
-        mlp_occ.basis_of_record = attributes_dict.get("Basis Of Record")
-        mlp_occ.collecting_method = attributes_dict.get("Collection Method")
-        mlp_occ.collector = attributes_dict.get("Collector")
-        mlp_occ.individual_count = attributes_dict.get("Count")
-        mlp_occ.item_description = attributes_dict.get("Description")
-        mlp_occ.in_situ = attributes_dict.get("In Situ")
-        mlp_occ.item_type = attributes_dict.get("Item Type")
-        mlp_occ.remarks = attributes_dict.get("Remarks")
-        mlp_occ.item_scientific_name = attributes_dict.get("Scientific Name")
-        field_number_datetime = datetime.datetime.strptime(attributes_dict.get("Time"),"%b %d, %Y, %H:%M %p" )
-        mlp_occ.field_number = field_number_datetime
-        pnt = GEOSGeometry("POINT (" + str(firstFeature.geometry.x) + " " + str(firstFeature.geometry.y) + ")") # WKT
-        mlp_occ.geom = pnt
-        mlp_occ.save()
+            mlp_occ.barcode = attributes_dict.get("Barcode")
+            mlp_occ.basis_of_record = attributes_dict.get("Basis Of Record")
+            mlp_occ.collecting_method = attributes_dict.get("Collection Method")
+            mlp_occ.collector = attributes_dict.get("Collector")
+            mlp_occ.individual_count = attributes_dict.get("Count")
+            mlp_occ.item_description = attributes_dict.get("Description")
+            mlp_occ.in_situ = attributes_dict.get("In Situ")
+            mlp_occ.item_type = attributes_dict.get("Item Type")
+            mlp_occ.remarks = attributes_dict.get("Remarks")
+            mlp_occ.item_scientific_name = attributes_dict.get("Scientific Name")
+            field_number_datetime = datetime.datetime.strptime(attributes_dict.get("Time"), "%b %d, %Y, %H:%M %p")
+            mlp_occ.field_number = field_number_datetime
+
+            utmPoint = utm.from_latlon(f.geometry.y, f.geometry.x)
+            pnt = GEOSGeometry("POINT (" + str(utmPoint[0]) + " " + str(utmPoint[1]) + ")", 32637) # WKT
+            mlp_occ.geom = pnt
+            mlp_occ.save()
 
         return super(UploadKMLView, self).form_valid(form)
 
