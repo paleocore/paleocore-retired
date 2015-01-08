09246f0440e284ae092ae07b4183fde672366a96
praomys <reedd@mail.utexas.edu>
2015-01-08 13:44:41 -0600

    Updated models

--- gdb/models.py
+++ gdb/models.py
@@ -5,18 +5,25 @@ from mysite.ontologies import BASIS_OF_RECORD_VOCABULARY, ITEM_TYPE_VOCABULARY, 
 
 class Occurrence(models.Model):
     specimen_number = models.IntegerField(primary_key=True)  # NOT NULL
-    cm_specimen_number = models.IntegerField()
+    cm_specimen_number = models.IntegerField(null=True, blank=True)  # CM SPec #
     locality = models.ForeignKey("Locality", to_field="locality_number")
-    date_collected = models.DateTimeField(blank=False, null=False, editable=True)  # NOT NULL
+    date_collected = models.DateField(blank=True, null=True, editable=True)
     time_collected = models.CharField(null=True, blank=True, max_length=50)
     date_last_modified = models.DateTimeField("Date Last Modified", auto_now_add=True, auto_now=True)
     basis_of_record = models.CharField("Basis of Record", max_length=50, blank=True, null=False,
                                        choices=BASIS_OF_RECORD_VOCABULARY)  # NOT NULL
+    item_scientific_name = models.CharField("Scientific Name", null=True, blank=True, max_length=255)  # Taxon
+    item_description = models.CharField(null=True, blank=True, max_length=255)
+    image = models.FileField(max_length=255, blank=True, upload_to="uploads/images/gdb", null=True)
+
     # Disposition fields
     loan_date = models.DateTimeField()
     loan_recipient = models.CharField(max_length=200)
-    on_loan = models.BooleanField(default=False)
-    geom = models.GeometryField(srid=4326, blank=True, null=True)  # NOT NULL
+    on_loan = models.BooleanField(default=False)  # Loan Status
+
+    # Geospatial
+    geom = models.GeometryField(srid=4326, blank=True, null=True)
+
 
     def __unicode__(self):
         """
@@ -32,13 +39,13 @@ class Biology(Occurrence):
     # Taxonomy fields
     kingdom = models.CharField(null=True, blank=True, max_length=50)
     phylum = models.CharField(null=True, blank=True, max_length=50)
-    tax_class = models.CharField("Class", null=True, blank=True, max_length=50)
-    tax_order = models.CharField("Order", null=True, blank=True, max_length=50)
+    tax_class = models.CharField("Class", null=True, blank=True, max_length=50)  # Class
+    tax_order = models.CharField("Order", null=True, blank=True, max_length=50)  # Order
     family = models.CharField(null=True, blank=True, max_length=50)
     subfamily = models.CharField(null=True, blank=True, max_length=50)
     tribe = models.CharField(null=True, blank=True, max_length=50)
     genus = models.CharField(null=True, blank=True, max_length=50)
-    specificepithet = models.CharField("Species Name", null=True, blank=True, max_length=50)
+    specificepithet = models.CharField("Species Name", null=True, blank=True, max_length=50)  # Species
     infraspecificepithet = models.CharField(null=True, blank=True, max_length=50)
     infraspecificrank = models.CharField(null=True, blank=True, max_length=50)
     # Identification fields
@@ -56,10 +63,12 @@ class Biology(Occurrence):
     element = models.CharField(null=True, blank=True, max_length=50)
     side = models.CharField(null=True, blank=True, max_length=50)
     attributes = models.CharField(null=True, blank=True, max_length=50)
-    faunanotes = models.TextField(null=True, blank=True, max_length=64000)
+    notes = models.TextField(null=True, blank=True, max_length=64000)
     lower_tooth = models.CharField(null=True, blank=True, max_length=50)
     upper_tooth = models.CharField(null=True, blank=True, max_length=50)
     jaw = models.CharField(null=True, blank=True, max_length=50)
+    mandible = models.CharField(null=True, blank=True, max_length=50)
+    maxilla = models.CharField(null=True, blank=True, max_length=50)
     teeth = models.CharField(null=True, blank=True, max_length=50)
     cranial = models.CharField(null=True, blank=True, max_length=50)
     miscellaneous = models.CharField(null=True, blank=True, max_length=50)
@@ -73,25 +82,26 @@ class Biology(Occurrence):
 class Locality(models.Model):
     locality_number = models.IntegerField(primary_key=True)  # NOT NULL
     locality_field_number = models.CharField(null=True, blank=True, max_length=50)
-    name = models.CharField(null=True, blank=True, max_length=50)
+    name = models.CharField(null=True, blank=True, max_length=50)  # Locality Name
     date_discovered = models.DateField(null=true, blank=True)
-    formation = models.CharField(null=True, blank=True, max_length=50)
+    formation = models.CharField(null=True, blank=True, max_length=50)  # Formation
     member = models.CharField(null=True, blank=True, max_length=50)
     NALMA = models.CharField(null=True, blank=True, max_length=50)
     survey = models.CharField(null=True, blank=True, max_length=50)
     quad_sheet = models.CharField(null=True, blank=True, max_length=50)
-    verbatim_latitude = models.CharField(null=True, blank=True, max_length=50)
-    verbatim_longitude = models.CharField(null=True, blank=True, max_length=50)
-    verbatim_utm = models.CharField(null=True, blank=True, max_length=50)
-    verbatim_gps_coordinates = models.CharField(null=True, blank=True, max_length=50)
-    verbatim_elevation = models.IntegerField(null=True, blank=True)
+    verbatim_latitude = models.CharField(null=True, blank=True, max_length=50)  # Latitude
+    verbatim_longitude = models.CharField(null=True, blank=True, max_length=50)  # Longitude
+    verbatim_utm = models.CharField(null=True, blank=True, max_length=50)  # UTM
+    verbatim_gps_coordinates = models.CharField(null=True, blank=True, max_length=50)  # GPS
+    verbatim_elevation = models.IntegerField(null=True, blank=True)  # Elevation
     gps_date = models.DateField(null=True, blank=True, editable=True)
     resource_area = models.CharField(null=True, blank=True, max_length=50)
     notes = models.CharField(null=True, blank=True, max_length=50)
-    cm_locality_number = models.IntegerField(null=True, blank=True)
+    cm_locality_number = models.IntegerField(null=True, blank=True)  # CM Loc #
     region = models.CharField(null=True, blank=True, max_length=50)
     blm_district = models.CharField(null=True, blank=True, max_length=50)
     county = models.CharField(null=True, blank=True, max_length=50)
+    image = models.FileField(max_length=255, blank=True, upload_to="uploads/images/gdb", null=True)
     geom = models.GeometryField(srid=4326, blank=True, null=True)  # NOT NULL
 
 
