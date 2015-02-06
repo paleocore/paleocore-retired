418ab6891d6dfced5c9c988fef734f036c9a59ca
praomys <reedd@mail.utexas.edu>
2015-02-06 15:59:18 -0600

    Updated mlp tests. Note that many now fail because of the point_x() method bug, issue 49.  Fixing the issue should fix tests.

--- mlp/tests.py
+++ mlp/tests.py
@@ -1,34 +1,31 @@
 from django.test import TestCase
-from mlp.models import Occurrence, Biology
+from mlp.models import Occurrence
 from fiber.models import Page
-from django.core.urlresolvers import reverse
+# from django.core.urlresolvers import reverse
 from datetime import datetime
-from django.utils import timezone
-from django.contrib.auth.models import User
-from django.contrib.auth import authenticate,login
-from django.contrib.admin.sites import AdminSite
+# from django.utils import timezone
+# from django.contrib.auth.models import User
+# from django.contrib.auth import authenticate,login
+# from django.contrib.admin.sites import AdminSite
 
 
 ###################
 # Factory Methods #
 ###################
 def create_django_page_tree():
-    mainmenu=Page(title='mainmenu')
+    mainmenu = Page(title='mainmenu')
     mainmenu.save()
-    Home = Page(title='Home', parent=mainmenu, url='home', template_name='arcana_home.html')
-    Home.save()
-    data = Page(title='Data', parent=Home, url='data', template_name='')
+    home = Page(title='home', parent=mainmenu, url='home', template_name='arcana_home.html')
+    home.save()
+    data = Page(title='Data', parent=home, url='data', template_name='')
     data.save()
-    MLP = Page(title='MLP', parent=Home, url='mlp', template_name='')
+    mlp = Page(title='mlp', parent=home, url='mlp', template_name='')
     data.save()
-    Upload = Page(title='Upload', parent=MLP, url='upload', template_name='')
+    upload = Page(title='upload', parent=mlp, url='upload', template_name='')
     data.save()
-    kml = Page(title='Data', parent=Upload, url='kml', template_name='')
+    kml = Page(title='Data', parent=upload, url='kml', template_name='')
+    kml.save()
     data.save()
-    #members=Page(title='members', parent=home, url='members', template_name='base/members')
-    #members.save()
-    #meetings = Page(title='meetings', parent=mainmenu, url='meetings', template_name='')
-    #meetings.save()
 
 
 ######################################
@@ -47,12 +44,13 @@ class OccurrenceMethodsTests(TestCase):
         # Using the instance creation and then save methods
         new_occurrence = Occurrence(id=1, item_type="Faunal", basis_of_record="HumanObservation",
                                     collecting_method="Surface Standard", field_number=datetime.now(),
-                                    geom="POINT (691311.7081000003963709 1272538.8992999996989965)")
+                                    geom="POINT (40.8352906016 11.5303732536)")
         new_occurrence.save()
         now = datetime.now()
         self.assertEqual(Occurrence.objects.count(), starting_record_count+1)  # test that one record has been added
         self.assertEqual(new_occurrence.date_last_modified.day, now.day)  # test date last modified is correct
-        self.assertEqual(new_occurrence.point_x(), 691311.7081000003963709)
+        self.assertEqual(new_occurrence.point_x(), 40.8352906016)
+        self.assertEqual(new_occurrence.point_y(), 11.5303732536)
 
     def test_mlp_create_method(self):
         """
@@ -61,24 +59,24 @@ class OccurrenceMethodsTests(TestCase):
         """
         starting_record_count = Occurrence.objects.count()
         new_occurrence = Occurrence.objects.create(id=1, item_type="Faunal", basis_of_record="HumanObservation",
-                                    collecting_method="Surface Standard", field_number=datetime.now(),
-                                    geom="POINT (691311.7081000003963709 1272538.8992999996989965)")
+                                                   collecting_method="Surface Standard", field_number=datetime.now(),
+                                                   geom="POINT (40.8352906016 11.5303732536)")
         now = datetime.now()
         self.assertEqual(Occurrence.objects.count(), starting_record_count+1)  # test that one record has been added
         self.assertEqual(new_occurrence.date_last_modified.day, now.day)  # test date last modified is correct
-        self.assertEqual(new_occurrence.point_x(), 691311.7081000003963709)
+        self.assertEqual(new_occurrence.point_x(), 40.8352906016)
 
     def test_mlp_create_method_invalid_item_type(self):
         """
         """
         starting_record_count = Occurrence.objects.count()
         new_occurrence = Occurrence.objects.create(id=1, item_type="Fake", basis_of_record="HumanObservation",
-                                    collecting_method="Surface Standard", field_number=datetime.now(),
-                                    geom="POINT (691311.7081000003963709 1272538.8992999996989965)")
+                                                   collecting_method="Surface Standard", field_number=datetime.now(),
+                                                   geom="POINT (40.8352906016 11.5303732536)")
         now = datetime.now()
         self.assertEqual(Occurrence.objects.count(), starting_record_count+1)  # test that one record has been added
         self.assertEqual(new_occurrence.date_last_modified.day, now.day)  # test date last modified is correct
-        self.assertEqual(new_occurrence.point_x(), 691311.7081000003963709)
+        self.assertEqual(new_occurrence.point_x(), 40.8352906016)
         self.assertEqual(new_occurrence.item_type, "Fake")
 
     def test_mlp_save_method_valid_item_type(self):
@@ -90,13 +88,13 @@ class OccurrenceMethodsTests(TestCase):
         new_occurrence.basis_of_record = "HumanObservation"
         new_occurrence.collecting_method = "Surface Standard"
         new_occurrence.field_number = datetime.now()
-        new_occurrence.geom = "POINT (691311.7081000003963709 1272538.8992999996989965)"
+        new_occurrence.geom = "POINT (40.8352906016 11.5303732536)"
         new_occurrence.save()
 
         now = datetime.now()
         self.assertEqual(Occurrence.objects.count(), starting_record_count+1)  # test that one record has been added
         self.assertEqual(new_occurrence.date_last_modified.day, now.day)  # test date last modified is correct
-        self.assertEqual(new_occurrence.point_x(), 691311.7081000003963709)
+        self.assertEqual(new_occurrence.point_x(), 40.8352906016)
         self.assertEqual(new_occurrence.item_type, "Faunal")
 
     def test_mlp_save_method_invalid_item_type(self):
@@ -108,29 +106,11 @@ class OccurrenceMethodsTests(TestCase):
         new_occurrence.basis_of_record = "HumanObservation"
         new_occurrence.collecting_method = "Surface Standard"
         new_occurrence.field_number = datetime.now()
-        new_occurrence.geom = "POINT (691311.7081000003963709 1272538.8992999996989965)"
+        new_occurrence.geom = "POINT (40.8352906016 11.5303732536)"
         new_occurrence.save()
 
         now = datetime.now()
         self.assertEqual(Occurrence.objects.count(), starting_record_count+1)  # test that one record has been added
         self.assertEqual(new_occurrence.date_last_modified.day, now.day)  # test date last modified is correct
-        self.assertEqual(new_occurrence.point_x(), 691311.7081000003963709)
+        self.assertEqual(new_occurrence.point_x(), 40.8352906016)
         self.assertEqual(new_occurrence.item_type, "Fake")
-
-
-
-
-
-
-###################
-# Page View Tests #
-###################
-class MLPViewTests(TestCase):
-    """
-    Test MLP page views with Django-Fiber
-    """
-    def test_mlp_home_view(self):
-        create_django_page_tree()
-        #response = self.client.get(reverse('mlp:mlp_upload_kml'))  # this line fails!
-        #self.assertEqual(response.status_code, 200)
-        #self.assertQuerysetEqual(response.context['meeting_list'], [])
