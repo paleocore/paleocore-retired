5a388dabdf4d5d1daffd3c02221c1a6bc16c8320
turban <tomislav.urban@gmail.com>
2015-02-17 09:01:19 -0600

    Issue #66. Adding additional secrets parameters.

--- mysite/settings.py
+++ mysite/settings.py
@@ -34,9 +34,8 @@ MEDIA_URL = "/media/"
 SECRET_KEY = secrets.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-
-TEMPLATE_DEBUG = True
+DEBUG = secrets.DEBUG_MODE
+TEMPLATE_DEBUG = secrets.TEMPLATE_DEBUG_MODE
 
 ALLOWED_HOSTS = [secrets.ALLOWED_HOSTS]
 
