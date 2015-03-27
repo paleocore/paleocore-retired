8a445c9200fe484b7c5c40f72e56ae25676c4a00
praomys <reedd@mail.utexas.edu>
2015-03-27 16:05:44 -0500

    updated models and admin

--- efossils/admin.py
+++ efossils/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
-from models import PaleoSite
-
+from models import PaleoSite, Occurrence
 
 # Register your models here.
-
-
+class OccurrenceAdmin(admin.ModelAdmin):
+    list_display = ('id', 'catalog_number', 'item_scientific_name', 'item_description')
 class PaleoSiteAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'setting')
 
 admin.site.register(PaleoSite, PaleoSiteAdmin)
+admin.site.register(Occurrence, OccurrenceAdmin)
