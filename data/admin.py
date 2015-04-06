16aa499219e89dbd19da4c2e6fc88e8c19c6c8ff
Denne Reed <denne.reed@gmail.com>
2015-04-06 16:49:14 -0500

    Formatting cleanup on data.admin

--- data/admin.py
+++ data/admin.py
@@ -8,7 +8,7 @@ class ProjectAdmin(admin.ModelAdmin):
     list_display = ('title', 'location', 'abstract', 'min_geochronological_age', "max_geochronological_age")
     list_display_links = ['title']
     list_filter = ['location', 'abstract']
-    search_fields = ['title','short_title','abstract','description','principle_investigator','location']
+    search_fields = ['title', 'short_title', 'abstract', 'description', 'principle_investigator', 'location']
 
 
 admin.site.register(Project, ProjectAdmin)
