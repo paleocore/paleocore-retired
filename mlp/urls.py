ba5280fb89169a69d54985da3eed842815e0ed7b
Denne Reed <denne.reed@gmail.com>
2015-07-28 13:04:34 -0500

    Updated urls.py to require login for the upload page. This incorporates the key element of Nick's pull request to address issue 90.

--- mlp/urls.py
+++ mlp/urls.py
@@ -1,12 +1,12 @@
 from django.conf.urls import patterns, url
 from mlp import views as mlp_views
-
+from django.contrib.auth.decorators import login_required
 
 urlpatterns = patterns('',
     # Project URLs are included by main urls.py
 
     # /projects/mlp/upload/
-    url(r'^upload/$', mlp_views.UploadKMLView.as_view(), name="mlp_upload_kml"),
+    url(r'^upload/$', login_required(mlp_views.UploadKMLView.as_view(), login_url='/login/'), name="mlp_upload_kml"),
 
     # /projects/mlp/download/
     url(r'^download/$', mlp_views.DownloadKMLView.as_view(), name="mlp_download_kml"),
