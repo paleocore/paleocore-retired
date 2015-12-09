3d84e8b4e4b325fdc33cb4a82bbec36e91745500
Denne Reed <denne.reed@gmail.com>
2015-12-09 16:20:28 -0600

    Updated drp admin to remove field_number readonly constraint. Fixed formatting.

--- drp/admin.py
+++ drp/admin.py
@@ -7,97 +7,92 @@ from django.core.exceptions import ObjectDoesNotExist
 from olwidget.admin import GeoModelAdmin
 
 
-#################
-## Media Admin ##
-#################
+###############
+# Media Admin #
+###############
 
 
-class imagesInline(admin.TabularInline):
+class ImagesInline(admin.TabularInline):
     model = Image
     extra = 0
     readonly_fields = ("id",)
 
 
-class filesInline(admin.TabularInline):
+class FilesInline(admin.TabularInline):
     model = File
     extra = 0
     readonly_fields = ("id",)
 
-###################
-## Biology Admin ##
-###################
+#################
+# Biology Admin #
+#################
 
-occurrence_fieldsets =(
-('Curatorial', {
-'fields': (('barcode','catalog_number'),
-           ("id", 'field_number', 'year_collected', 'date_last_modified'),
-           ("collection_code", "paleolocality_number", "item_number", "item_part"))
-}),
+occurrence_fieldsets = (
+    ('Curatorial', {
+        'fields': (('barcode', 'catalog_number'),
+                   ("id", 'field_number', 'year_collected', 'date_last_modified'),
+                   ("collection_code", "paleolocality_number", "item_number", "item_part"))
+    }),
 
-('Occurrence Details', {
-'fields': (('basis_of_record', 'item_type', 'disposition', 'preparation_status'),
-           ('item_description', 'item_scientific_name'),
-           ('remarks'))
-}),
-('Provenience', {
-'fields': (("stratigraphic_marker_upper", "distance_from_upper"),
-           ("stratigraphic_marker_lower", "distance_from_lower"),
-           ("stratigraphic_marker_found", "distance_from_found"),
-           ("stratigraphic_marker_likely", "distance_from_likely"),
-           ("analytical_unit", "analytical_unit_2", "analytical_unit_3"),
-           ("in_situ", "ranked"),
-           ("stratigraphic_member",),
-           ('geom'))
-}),
-)
-
-
-biology_fieldsets = (
-    ('Taxonomy', {'fields':
-                      (('taxon',),
-                       ('id')
-                      )
+    ('Occurrence Details', {
+        'fields': (('basis_of_record', 'item_type', 'disposition', 'preparation_status'),
+                   ('item_description', 'item_scientific_name'), 'remarks')
+    }),
+    ('Provenience', {
+        'fields': (("stratigraphic_marker_upper", "distance_from_upper"),
+                   ("stratigraphic_marker_lower", "distance_from_lower"),
+                   ("stratigraphic_marker_found", "distance_from_found"),
+                   ("stratigraphic_marker_likely", "distance_from_likely"),
+                   ("analytical_unit", "analytical_unit_2", "analytical_unit_3"),
+                   ("in_situ", "ranked"),
+                   ("stratigraphic_member",), 'geom')
     }),
 )
 
+biology_fieldsets = (
+    ('Taxonomy', {'fields': (('taxon',), 'id')
+                  }),
+)
 
-class biologyInline(admin.TabularInline):
+
+class BiologyInline(admin.TabularInline):
     model = Biology
     extra = 0
     readonly_fields = ("id",)
     fieldsets = biology_fieldsets
 
 
-class biologyAdmin(admin.ModelAdmin):
+class BiologyAdmin(admin.ModelAdmin):
     list_display = ("id", "collection_code", "paleolocality_number", "item_number", "item_part",
                     'stratigraphic_member', "barcode", 'basis_of_record', 'item_type', 'taxon', )
     list_filter = ("family",)
     readonly_fields = ("id",)
     fieldsets = biology_fieldsets
 
-    #note: autonumber fields like id are not editable, and can't be added to fieldsets unless specified as read only.
-    #also, any dynamically created fields (e.g. point_X) in models.py must be declared as read only to be included in fieldset or fields
+    # note: autonumber fields like id are not editable, and can't be added to fieldsets unless specified as read only.
+    # also, any dynamically created fields (e.g. point_X) in models.py must be declared as read only to be included
+    # in fieldset or fields
     readonly_fields = ("id", "field_number", "catalog_number", "date_last_modified")
 
     list_filter = ["basis_of_record", "year_collected", "stratigraphic_member", "collection_code", "item_type"]
     search_fields = ("id", "item_scientific_name", "barcode", "catalog_number")
-    inlines = (biologyInline, imagesInline, filesInline)
+    inlines = (BiologyInline, ImagesInline, FilesInline)
     fieldsets = occurrence_fieldsets
     formfield_overrides = {
         models.CharField: {'widget': TextInput(attrs={'size':'25'})},
         models.TextField: {'widget': Textarea(attrs={'rows': 4, 'cols': 40})},
     }
     list_per_page = 500  # show 500 records per page
-    #change_form_template = "occurrence_change_form.html"
+    # change_form_template = "occurrence_change_form.html"
     actions = ["move_selected", "get_nearest_locality"]  # TODO clarify actions
     actions = ["get_nearest_locality", "create_data_csv"]
 
 
-#####################
-## Hydrology Admin ##
-#####################
+###################
+# Hydrology Admin #
+###################
 
-class hydrologyAdmin(GeoModelAdmin):
+class HydrologyAdmin(GeoModelAdmin):
     list_display = ("id", "size")
     search_fields = ("id",)
     list_filter = ("size",)
@@ -107,11 +102,11 @@ class hydrologyAdmin(GeoModelAdmin):
     }
 
 
-#####################
-## Locality Admin ##
-#####################
+##################
+# Locality Admin #
+##################
 
-class localityAdmin(GeoModelAdmin):
+class LocalityAdmin(GeoModelAdmin):
     list_display = ("collection_code", "paleolocality_number", "paleo_sublocality")
     list_filter = ("collection_code",)
     search_fields = ("paleolocality_number",)
@@ -120,31 +115,31 @@ class localityAdmin(GeoModelAdmin):
         'layers': ['google.terrain']
     }
 
-######################
-## Occurrence Admin ##
-######################
+####################
+# Occurrence Admin #
+####################
 
 
-class occurrenceAdmin(GeoModelAdmin):
-    list_display = ("id", "collection_code", "paleolocality_number", "item_number", "item_part",'stratigraphic_member', "barcode", 'basis_of_record', 'item_type',
+class OccurrenceAdmin(GeoModelAdmin):
+    list_display = ("id", "collection_code", "paleolocality_number", "item_number", "item_part", 'stratigraphic_member',
+                    "barcode", 'basis_of_record', 'item_type',
                     "item_scientific_name", "item_description", "year_collected")
 
-
-    #note: autonumber fields like id are not editable, and can't be added to fieldsets unless specified as read only.
-    #also, any dynamically created fields (e.g. point_X) in models.py must be declared as read only to be included in fieldset or fields
-    readonly_fields = ("id","field_number", "catalog_number", "date_last_modified")
+    # note: autonumber fields like id are not editable, and can't be added to fieldsets unless specified as read only.
+    # also, any dynamically created fields (e.g. point_X) in models.py must be declared as read only to be included in
+    # fieldset or fields
+    readonly_fields = ("id", "catalog_number", "date_last_modified")
 
     list_filter = ["basis_of_record", "year_collected", "stratigraphic_member", "collection_code", "item_type"]
     search_fields = ("id", "item_scientific_name", "barcode", "catalog_number")
-    inlines = (biologyInline, imagesInline, filesInline)
+    inlines = (BiologyInline, ImagesInline, FilesInline)
     fieldsets = occurrence_fieldsets
     formfield_overrides = {
-        models.CharField: {'widget': TextInput(attrs={'size':'25'})},
+        models.CharField: {'widget': TextInput(attrs={'size': '25'})},
         models.TextField: {'widget': Textarea(attrs={'rows': 4, 'cols': 40})},
     }
     list_per_page = 500  # show 500 records per page
-    #change_form_template = "occurrence_change_form.html"
-    actions = ["move_selected", "get_nearest_locality"]  # TODO clarify actions
+    # change_form_template = "occurrence_change_form.html"
     actions = ["get_nearest_locality", "create_data_csv"]
 
     options = {
@@ -204,78 +199,55 @@ class occurrenceAdmin(GeoModelAdmin):
 
     create_data_csv.short_description = "Download Selected to .csv"
 
-    #admin action to get nearest locality
-    def get_nearest_locality(self,request, queryset):
-        #first make sure we are only dealing with one point
-        if queryset.count()>1:
-            self.message_user(request,"You can't get the nearest locality for multiple points at once. Please select a single point.",level='error')
+    # admin action to get nearest locality
+    def get_nearest_locality(self, request, queryset):
+        # first make sure we are only dealing with one point
+        if queryset.count() > 1:
+            self.message_user(request, "You can't get the nearest locality for multiple points at once. "
+                                       "Please select a single point.", level='error')
             return
-        #check if point is within any localities
+        # check if point is within any localities
         matching_localities = []
         for locality in Locality.objects.all():
             if locality.geom.contains(queryset[0].geom):
                 matching_localities.append(str(locality.collection_code) + "-" + str(locality.paleolocality_number))
         if matching_localities:
-            #warning to user if the point is within multiple localities
-            if len(matching_localities)>1:
-                self.message_user(request,"The point falls within multiple localities (localities %s). Please consider redifining your localities so they don't overlap."% str(matching_localities).replace("[",""))
+            # warning to user if the point is within multiple localities
+            if len(matching_localities) > 1:
+                self.message_user(request, "The point falls within multiple localities (localities %s). "
+                                           "Please consider redefining your localities so they don't overlap."
+                                  % str(matching_localities).replace("[", ""))
                 return
-            #Message user with the nearest locality
-            self.message_user(request,"The point is in %s" %(matching_localities[0]))
+            # Message user with the nearest locality
+            self.message_user(request, "The point is in %s" % (matching_localities[0]))
 
-        #if the point is not within any locality, get the nearest locality
-        distances={}#dictionary which will contain {<localityString>:key} entries
+        # if the point is not within any locality, get the nearest locality
+        distances = {}  # dictionary which will contain {<localityString>:key} entries
         for locality in Locality.objects.all():
-            locality_name=str(locality.collection_code) + "-" + str(locality.paleolocality_number)
-            #how are units being dealt with here?
+            locality_name = str(locality.collection_code) + "-" + str(locality.paleolocality_number)
+            #  how are units being dealt with here?
             locality_distance_from_point = locality.geom.distance(queryset[0].geom)
-            distances.update({locality_name:locality_distance_from_point})
-            closest_locality_key=min(distances,key=distances.get)
-        self.message_user(request,"The point is %d meters from locality %s" %(distances.get(closest_locality_key),closest_locality_key))
+            distances.update({locality_name: locality_distance_from_point})
+            closest_locality_key = min(distances, key=distances.get)
+        self.message_user(request, "The point is %d meters from locality %s" % (distances.get(closest_locality_key),
+                                                                                closest_locality_key))
 
 
-    #admin action to move points to specified x and y coordinates
-    # TODO test and implement. Not currently active.
-    def move_selected(self,request,queryset):
-        returnURL="/admin/drp/occurrence/"
-        def render_move_form():
-            t=loader.get_template("move_points.html")
-            c = RequestContext(request, {'returnURL': returnURL, 'selectedPoints': queryset, 'action_checkbox_name': helpers.ACTION_CHECKBOX_NAME,})
-            return HttpResponse(t.render(c))
+###################
+# Taxonomy Admin  #
+###################
 
-        if "apply" in request.POST:#if move form has been completed
-            if request.POST["NewX"]:
-                if request.POST["NewY"]:
-                    for point in queryset:
-                        point.geom.x=float(request.POST["NewX"])
-                        point.geom.y=float(request.POST["NewY"])
-                        point.save()
-                        self.message_user(request,"Point Succesfully Moved")
-                else:
-                    return render_move_form()
-            else:
-                return render_move_form()
-        else:#if move form has NOT been completed
-            return render_move_form()
-    move_selected.short_description = "Move the selected points."
-
-
-#####################
-## Taxonomy Admin  ##
-#####################
-
-class taxonomyAdmin(admin.ModelAdmin):
+class TaxonomyAdmin(admin.ModelAdmin):
     list_display = ("id", "rank", "taxon")
     search_fields = ("taxon",)
     list_filter = ("rank",)
 
 
-############################
-## Register Admin Classes ##
-############################
+##########################
+# Register Admin Classes #
+##########################
 
-admin.site.register(Biology, biologyAdmin)
-admin.site.register(Hydrology, hydrologyAdmin)
-admin.site.register(Locality, localityAdmin)
-admin.site.register(Occurrence, occurrenceAdmin)
-
+admin.site.register(Biology, BiologyAdmin)
+admin.site.register(Hydrology, HydrologyAdmin)
+admin.site.register(Locality, LocalityAdmin)
+admin.site.register(Occurrence, OccurrenceAdmin)
