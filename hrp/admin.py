e5f1c70961fdaef1bfd0815dd541a840b3b0974e
Denne Reed <denne.reed@gmail.com>
2016-08-16 20:51:29 -0700

    Added dental import script. Added dental fields to Biology model.

--- hrp/admin.py
+++ hrp/admin.py
@@ -169,18 +169,76 @@ class OccurrenceAdmin(base.admin.PaleoCoreOccurrenceAdmin):
 ###################
 
 class TaxonomyAdmin(admin.ModelAdmin):
-    list_display = ("id", "rank", "taxon")
+    list_display = ("id", "rank", "taxon", "full_lineage")
     search_fields = ("taxon",)
     list_filter = ("rank",)
+    readonly_fields = "full_lineage"
 
 #################
 # Biology Admin #
 #################
 
 
+biology_inline_fieldsets = (
+    ('Taxonomy', {'fields': (('taxon',), 'id')}),
+)
+
+biology_element_fieldsets = (
+    ('Elements', {'fields': (
+        ('element', 'element_modifier'),
+        ('uli1', 'uli2', 'ulc', 'ulp3', 'ulp4', 'ulm1', 'ulm2', 'ulm3'),
+        ('uri1', 'uri2', 'urc', 'urp3', 'urp4', 'urm1', 'urm2', 'urm3'),
+        ('lri1', 'lri2', 'lrc', 'lrp3', 'lrp4', 'lrm1', 'lrm2', 'lrm3'),
+        ('lli1', 'lli2', 'llc', 'llp3', 'llp4', 'llm1', 'llm2', 'llm3'),
+        ('indet_incisor', 'indet_canine', 'indet_premolar', 'indet_molar', 'indet_tooth'),
+        'deciduous'
+                           )}),
+)
+
+
 biology_fieldsets = (
-    ('Taxonomy', {'fields': (('taxon',), 'id')
-                  }),
+    ('Record Details', {
+        'fields': [('id', 'date_last_modified',)]
+    }),
+    ('Item Details', {
+        'fields': [('barcode', 'catalog_number',),
+                   ('date_recorded', 'year_collected',),
+                   ("collection_code", "locality", "item_number", "item_part")]
+    }),
+
+    ('Occurrence Details', {
+        'fields': [('basis_of_record', 'item_type', 'disposition', 'preparation_status'),
+                   ('collecting_method', 'finder', 'collector', 'individual_count'),
+                   ('item_description', 'item_scientific_name', 'image'),
+                   ('problem', 'problem_comment'),
+                   ('remarks',)]
+    }),
+    biology_element_fieldsets[0],
+    ('Geological Context', {
+        'fields': [('stratigraphic_marker_upper', 'distance_from_upper'),
+                   ('stratigraphic_marker_lower', 'distance_from_lower'),
+                   ('stratigraphic_marker_found', 'distance_from_found'),
+                   ('stratigraphic_marker_likely', 'distance_from_likely'),
+                   ('analytical_unit', 'analytical_unit_2', 'analytical_unit_3'),
+                   ('in_situ', 'ranked'),
+                   ('stratigraphic_member',),
+                   ('locality', 'drainage_region')]
+    }),
+
+    ('Location Details', {
+        'fields': [('longitude', 'latitude'),
+                   ('easting', 'northing',),
+                   ('geom',)]
+    }),
+    # ('Elements', {'fields': (
+    #     ('element', 'element_modifier'),
+    #     ('uli1', 'uli2', 'ulc', 'ulp3', 'ulp4', 'ulm1', 'ulm2', 'ulm3'),
+    #     ('uri1', 'uri2', 'urc', 'urp3', 'urp4', 'urm1', 'urm2', 'urm3'),
+        # ('uri1', 'uri2', 'urc', 'urp3', 'urp4', 'urm1', 'urm2', 'urm3'),
+        # ('uli1', 'uli2', 'ulc', 'ulp3', 'ulp4', 'ulm1', 'ulm2', 'ulm3'),
+        # ('indet_incisor', 'indet_canine', 'indet_premolar', 'indet_molar', 'indet_tooth'),
+        # 'deciduous'
+                          #)}),
 )
 
 
@@ -188,12 +246,18 @@ class BiologyInline(admin.TabularInline):
     model = Biology
     extra = 0
     readonly_fields = ("id",)
-    fieldsets = biology_fieldsets
+    fieldsets = biology_inline_fieldsets
+
+
+class ElementInLine(admin.StackedInline):
+    model = Biology
+    extra = 0
+    fieldsets = biology_element_fieldsets
 
 
 class BiologyAdmin(OccurrenceAdmin):
+    fieldsets = biology_fieldsets
     inlines = (BiologyInline, ImagesInline, FilesInline)
-
 
 
 class ArchaeologyAdmin(OccurrenceAdmin):
@@ -202,6 +266,7 @@ class ArchaeologyAdmin(OccurrenceAdmin):
 
 class GeologyAdmin(OccurrenceAdmin):
     pass
+
 
 class BiologyAdmin_old(admin.ModelAdmin):
     list_display = ("id", "collection_code", "locality", "item_number", "item_part",
